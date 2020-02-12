00a0bd7def52e3f09d04f14d727b1c9ecfdcf841
MindF <MindFlare56@hotmail.fr>
2020-02-12 07:52:24 -0500

    asd

--- Kanu2/Controllers/HomeController.cs
+++ Kanu2/Controllers/HomeController.cs
@@ -19,12 +19,5 @@ namespace Kanu2.Controllers
 
             return View();
         }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
